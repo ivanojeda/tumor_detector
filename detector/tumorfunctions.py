eb6ae1c2f233224ab9ac050931a9fb47cce348bf
isdl29 <ivansaizdelucasso@gmail.com>
2022-07-27 18:06:28 +0200

    cosa

--- detector/tumorfunctions.py
+++ detector/tumorfunctions.py
@@ -9,25 +9,16 @@ RESUNET_PATH='resunet.h5'
 RESNET_BACKUP_PATH='resnet_backup.h5'
 RESUNET_BACKUP_PATH='resunet_backup.h5'
 
-def __loadimage(img_binstr):
-    image_np = np.from_buffer(img_binstr, dtype=np.uint8) # Get a 1d-array of an image
-    imagetensor=cv2.imdecode(image_np, cv2.IMREAD_COLOR)
-    return imagetensor
-
-def __saveimage(imagetensor):
-    imagedata=imagetensor.dumps()
-    return imagedata
-
-def tpredict(imagedata):
-    image=__loadimage(imagedata)
+def tpredict(inputImgPath,outputImgPath):
+    image=cv2.imread(inputImgPath)
     model1=load_model(RESNET_PATH)
-    pred1=model1.predict(imagetensor)
+    pred1=model1.predict(image)
     if pred1<0.5:
-        pred2=np.zeros(shape=(256,256,3))
+        pred2=np.zeros(shape=image.shape)
     else:
         model2=load_model(RESUNET_PATH)
-        pred2=model2.predict(imagetensor)
-        return __saveimage(pred2)
+        pred2=model2.predict(image)
+    return cv2.imwrite(outputImgPath,pred2)
           
 
         
