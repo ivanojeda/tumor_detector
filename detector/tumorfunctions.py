4b108915b9d2dfd0b984b48b87e4a88285d51532
isdl29 <ivansaizdelucasso@gmail.com>
2022-07-28 15:08:35 +0200

    cosa

--- detector/tumorfunctions.py
+++ detector/tumorfunctions.py
@@ -1,23 +1,27 @@
 import numpy as np
 import cv2
 import tensorflow as tf
-from tf.keras.models import load_model
+from tensorflow.keras.models import load_model
 
-RESNET_PATH='resnet.h5'
-RESUNET_PATH='resunet.h5'
+RESNET_PATH='IA/resnet.h5'
+RESUNET_PATH='IA/resunet.h5'
 
-RESNET_BACKUP_PATH='resnet_backup.h5'
-RESUNET_BACKUP_PATH='resunet_backup.h5'
+RESNET_BACKUP_PATH='IA/resnet_backup.h5'
+RESUNET_BACKUP_PATH='IA/resunet_backup.h5'
+
+# Meter aquí las funciones tversky o lo que sea que use la resunet.
+
+resunet_objects=None
 
 def tpredict(inputImgPath,outputImgPath):
     image=cv2.imread(inputImgPath)
-    model1=load_model(RESNET_PATH)
-    pred1=model1.predict(image)
-    if pred1<0.5:
-        pred2=np.zeros(shape=image.shape)
+    resnet=load_model(RESNET_PATH)
+    tumor=resnet.predict(image)
+    if tumor<0.5:
+        mask=np.zeros(shape=image.shape)
     else:
-        model2=load_model(RESUNET_PATH)
-        pred2=model2.predict(image)
+        resunet=load_model(RESUNET_PATH,custom_objects=)
+        mask=resunet.predict(image)
     return cv2.imwrite(outputImgPath,pred2)
           
 
