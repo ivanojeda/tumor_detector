c9140ac70dd32bd68a39e51a908bb850aece69ea
isdl29 <ivansaizdelucasso@gmail.com>
2022-08-01 17:39:06 +0200

    cosa

--- detector/tumorfunctions.py
+++ detector/tumorfunctions.py
@@ -17,6 +17,7 @@ RESUNET_BACKUP_PATH='detector\\IA\\resunet_backup.h5'
 resunet_objects={'tversky':tversky,'focal_tversky_loss':focal_tversky_loss}
 
 def getOutputPath(inputPath):
+    '''Genera el directorio de la predicción a partir de la imagen a analizar.'''
     inpathsp=inputPath.split('.')
     return inpathsp[0]+'_P.'+inpathsp[1]
 
@@ -28,17 +29,24 @@ def resunetImg(img):
     return opimg[np.newaxis,:,:,:]
 
 def tpredict(inputImgPath,debug=False):
+    '''Ejecuta la predicción.
+    1) Comprueba si la imagen en 256x256, si no, reajusta la imagen a ese tamaño.
+    2) Predice con la resnet si la imagen tiene tumor o no y se guarda el resultado.
+    3) Dependiendo del resultado anterior:
+    - Si tiene tumor, se pasa la imagen a la resunet para que devuelva la máscara del tumor predicho.
+    - Si no tiene tumor, se genera una máscara vacía.
+    4) Se junto la imagen con su máscara para generar la predicción y se guarda en la base de datos.
+    Además, retorna un diccionario con las siguientes variables:
+    - tumor: Si tiene tumor o no (0 o 1)
+    - opimg: el directorio de la predicción.'''
     outputImgPath=getOutputPath(inputImgPath)
     inputImg=cv2.imread(inputImgPath)
     if inputImg.shape !=(256,256,3):
         inputImg = cv2.resize(inputImg ,dsize=(256,256),interpolation=cv2.INTER_NEAREST)
     result={}
-    try:
-        resnet=load_model(RESNET_PATH)
-        tumor=resnet.predict(resnetImg(inputImg))[0][0]
-        print(tumor)
-    except:
-        tumor=1
+    resnet=load_model(RESNET_PATH)
+    tumor=resnet.predict(resnetImg(inputImg))[0][0]
+    #print(tumor)
     if tumor<0.5:
         result['tumor']=False
         mask=np.zeros(shape=inputImg.shape)
